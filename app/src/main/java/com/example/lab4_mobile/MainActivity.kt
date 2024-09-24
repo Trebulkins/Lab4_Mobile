664774a85632153a02890609ec67a9452ca1a644
Trebulkins <trebinivan2005@gmail.com>
2024-09-24 17:41:32 +0700

    it works!

--- app/src/main/java/com/example/lab4_mobile/MainActivity.kt
+++ app/src/main/java/com/example/lab4_mobile/MainActivity.kt
@@ -22,38 +22,84 @@ class MainActivity : AppCompatActivity() {
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
             insets
         }
-        val questions = listOf("Вопрос 1", "Вопрос 2", "ВОПРОС 3!")
+        val questions = listOf(
+            "Может ли бегемот бежать со скоростью в 30 км/ч?.",
+            "Вопрос 2",
+            "ВОПРОС 3!"
+        )
+        val answers = listOf(
+            true,
+            false,
+            false
+        )
+
         val yesb = findViewById<Button>(R.id.YESbutton)
         val nob = findViewById<Button>(R.id.NObutton)
         val nextb = findViewById<Button>(R.id.NEXTbutton)
         val que = findViewById<TextView>(R.id.Question)
+        val queN = findViewById<TextView>(R.id.TextNum)
+        val restart = findViewById<TextView>(R.id.RESTARTbutton)
+        val resultscr = findViewById<TextView>(R.id.ResultScreen)
 
         var i = 0
+        var score = 0
+
 
         nextb.visibility = View.INVISIBLE
+        restart.visibility = View.INVISIBLE
+        resultscr.visibility = View.INVISIBLE
         que.text = questions[i]
+        queN.text = "Вопрос №${i + 1}:"
         yesb.setOnClickListener {
-            Toast.makeText(this, "АГА!", Toast.LENGTH_SHORT).show()
+            if (answers[i]){
+                score++
+            }
             nextb.visibility = View.VISIBLE
             yesb.visibility = View.INVISIBLE
             nob.visibility = View.INVISIBLE
         }
 
         nob.setOnClickListener {
-            Toast.makeText(this, "НЕА!", Toast.LENGTH_SHORT).show()
+            if (!answers[i]){
+                score++
+            }
             nextb.visibility = View.VISIBLE
             yesb.visibility = View.INVISIBLE
             nob.visibility = View.INVISIBLE
         }
 
         nextb.setOnClickListener {
-            Toast.makeText(this, "СЛЕДУЮЩИЙ ВОПРОС", Toast.LENGTH_SHORT).show()
-            nextb.visibility = View.INVISIBLE
-            yesb.visibility = View.VISIBLE
-            nob.visibility = View.VISIBLE
             i++
-            que.text = questions[i]
+            if (i < questions.size) {
+                nextb.visibility = View.INVISIBLE
+                yesb.visibility = View.VISIBLE
+                nob.visibility = View.VISIBLE
+                que.text = questions[i]
+                queN.text = "Вопрос №${i + 1}:"
+            }
+            else {
+                nextb.visibility = View.INVISIBLE
+                yesb.visibility = View.INVISIBLE
+                nob.visibility = View.INVISIBLE
+                que.visibility = View.INVISIBLE
+                queN.visibility = View.INVISIBLE
+                restart.visibility = View.VISIBLE
+                resultscr.text = "РЕЗУЛЬТАТ: ${score}/${questions.size}"
+                resultscr.visibility = View.VISIBLE
+            }
         }
 
+        restart.setOnClickListener {
+            i = 0
+            score = 0
+            que.text = questions[i]
+            queN.text = "Вопрос №${i + 1}:"
+            yesb.visibility = View.VISIBLE
+            nob.visibility = View.VISIBLE
+            que.visibility = View.VISIBLE
+            queN.visibility = View.VISIBLE
+            restart.visibility = View.INVISIBLE
+            resultscr.visibility = View.INVISIBLE
+        }
     }
 }
